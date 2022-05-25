b1ed007dcc2404c1adef14cf7340edef185ae549
stalukder03 <stalukder03@gmail.com>
2022-05-25 11:46:45 +0600

    Create First component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Gretting() {
+	return <h1>Welcome to React basic practice.</h1>;
+}
+
+ReactDOM.render(<Gretting />, document.getElementById('root'));
