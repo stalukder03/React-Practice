a520c9b509b73974e74ac75f72c1c70f6f36f496
stalukder03 <stalukder03@gmail.com>
2022-05-25 12:15:02 +0600

    JSX Rules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// JSX Rules
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element //cee
+// formatting
+
 function Gretting() {
-	return <h1>Welcome to React basic practice.</h1>;
+	return (
+		<>
+			<h1>Welcome to React basic practice.</h1>
+			<input type="text" name="" id="" />
+		</>
+	);
 }
 
 ReactDOM.render(<Gretting />, document.getElementById('root'));
